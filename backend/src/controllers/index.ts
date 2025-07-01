a6f61b1508979bea7a6040ea1efcc677c9d4c01a
InfiniteALioth <aliothzhou@gmail.com>
2025-07-01 22:33:01 +0800

    修复后端目录结构和依赖问题

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,8 @@
+// 控制器入口文件
+export * from './authController';
+export * from './pageController';
+export * from './mediaController';
+export * from './chatController';
+export * from './userController';
+export * from './uploadController';
+export * from './adminController';
